5c39dda04a884950e2aee1f69d76901693caa9bc
Marie Claire LeBlanc Flanagan <marieflanagan@gmail.com>
2024-05-20 00:23:33 -0400

    new osc recording working

--- Assets/Scripts/OSCMessages.cs
+++ Assets/Scripts/OSCMessages.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-// monobehaviour means that it will expose public variables and methods
 public class oscMessages : MonoBehaviour
 {
     Vector3 osc;
@@ -17,16 +16,20 @@ public class oscMessages : MonoBehaviour
     void Update()
     {
         transform.position = osc;
-        // Debug.Log(osc);
     }
 
-    public void ReceiveOSC(Vector3 incomingOSCValues){
-        osc.x = MapToRange(incomingOSCValues.x, 0, 640, -4, 4);
-        osc.y = MapToRange(incomingOSCValues.y, 0, 480, 8, -4);
-        osc.z = 0f;
+    public void ReceiveOSC(Vector3 incomingOSCValues)
+    {
+        Debug.Log($"Received OSC values: {incomingOSCValues.x}, {incomingOSCValues.y}");
+        osc.x = MapToRange(incomingOSCValues.x, 0, 640, -14, 14);
+        osc.z = MapToRange(incomingOSCValues.y, 0, 480, -14, 14); 
+        osc.y = 0f; // ground plane
+
+        // osc.y =  the camera perspective or height of the avatar
     }
 
-    public float MapToRange(float numberInRange1, float start1, float end1, float start2, float end2) {
+    public float MapToRange(float numberInRange1, float start1, float end1, float start2, float end2)
+    {
         float distance = numberInRange1 - start1;
         float distanceRatio = distance / (end1 - start1);
         float amountInRange2 = distanceRatio * (end2 - start2);
