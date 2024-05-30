c1ea47f15f77928df72d7dce9d618c8fb54c6e8c
cabbibo <isaaclandoncohen@gmail.com>
2024-05-30 02:38:51 -0700

    more push

--- Assets/Hug.cs
+++ Assets/Hug.cs
@@ -6,8 +6,8 @@ public class Hug : MonoBehaviour
 {
 
     public int numFacePairs;
-    public float faceSize;
     public GameObject facePrefab;
+    public float faceSize;
     public float faceVerticality;
     public List<HugFace> faces;
 
@@ -30,13 +30,14 @@ public class Hug : MonoBehaviour
         {
             HugFace face1 = Instantiate(facePrefab).GetComponent<HugFace>();
             face1.transform.position = controller.getFinalPosition(new Vector3(Random.Range(-1f, 1f), Random.Range(0, 1f), Random.Range(-1f, 1f)));
-            face1.transform.localScale = Vector3.one * faceSize;
+            face1.transform.localScale = Vector3.one * faceSize * 2;
+            face1.transform.parent = transform;
 
 
             HugFace face2 = Instantiate(facePrefab).GetComponent<HugFace>();
             face2.transform.position = controller.getFinalPosition(new Vector3(Random.Range(-1f, 1f), Random.Range(0, 1f), Random.Range(-1f, 1f)));
-            face2.transform.localScale = Vector3.one * faceSize;
-
+            face2.transform.localScale = Vector3.one * faceSize * 2;
+            face2.transform.parent = transform;
 
             face1.hug = this;
             face2.hug = this;
@@ -44,8 +45,8 @@ public class Hug : MonoBehaviour
             face1.smileID = i;
             face2.smileID = i;
 
-            face1.partner = face2.gameObject;
-            face2.partner = face1.gameObject;
+            face1.partner = face2;
+            face2.partner = face1;
 
             face1.gameObject.SetActive(true);
             face2.gameObject.SetActive(true);
@@ -61,6 +62,11 @@ public class Hug : MonoBehaviour
 
     public void Update()
     {
+        for (int j = 0; j < faces.Count; j++)
+        {
+
+            faces[j].WhileOutside();
+        }
 
         for (int i = 0; i < controller.activePlayers.Count; i++)
         {
@@ -77,17 +83,76 @@ public class Hug : MonoBehaviour
                     closestFaceID = j;
                 }
 
+
             }
 
 
             if (closestFaceDistance < activationRadius)
             {
 
+                faces[closestFaceID].WhileInside();
 
             }
 
 
         }
+
+        if (completedFaces != null)
+        {
+            for (int i = 0; i < completedFaces.Count; i++)
+            {
+                completedFaces[i].WhileFinished();
+            }
+        }
+
+    }
+
+    public GameObject connectionPrefab;
+    public List<LineRenderer> connections;
+    public List<int> smilesDiscovered;
+
+    public List<HugFace> completedFaces;
+
+    public void PairDiscover(GameObject f1, GameObject f2, int smileID)
+    {
+
+        if (smilesDiscovered == null)
+        {
+            smilesDiscovered = new List<int>();
+        }
+
+        if (connections == null)
+        {
+            connections = new List<LineRenderer>();
+        }
+
+        if (smilesDiscovered.Contains(smileID))
+        {
+            return;
+        }
+
+        if (completedFaces == null)
+        {
+            completedFaces = new List<HugFace>();
+        }
+
+        completedFaces.Add(f1.GetComponent<HugFace>());
+        completedFaces.Add(f2.GetComponent<HugFace>());
+
+
+
+        smilesDiscovered.Add(smileID);
+
+        print("YAY");
+
+
+        LineRenderer connection = Instantiate(connectionPrefab).GetComponent<LineRenderer>();
+
+        connection.gameObject.transform.parent = transform;
+
+        connection.SetPosition(0, f1.transform.position);
+        connection.SetPosition(1, f2.transform.position);
+        connections.Add(connection);
 
     }
 
--- Assets/StickTogether.cs
+++ Assets/StickTogether.cs
@@ -9,7 +9,17 @@ public class StickTogether : MonoBehaviour
     public float radiusForCollection = 1;
 
     public GameObject collectionRepresentation;
-    public List<GameObject> connections;
+    public List<LineRenderer> connections;
+    public GameObject connectionPrefab;
+
+    public int totalInsideCircle = 0;
+
+
+    public Vector3 movementSpeed;
+    public Vector3 movementSize;
+
+    public Vector3 movementOffset;
+
 
 
 
@@ -22,6 +32,62 @@ public class StickTogether : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        transform.localScale = new Vector3(radiusForCollection * 2, radiusForCollection * 2, radiusForCollection * 2);
+
+
+        Vector3 position = new Vector3(
+            Mathf.Sin(Time.time * movementSpeed.x) * movementSize.x,
+            Mathf.Sin(Time.time * movementSpeed.y) * movementSize.y,
+            Mathf.Sin(Time.time * movementSpeed.z) * movementSize.z
+        );
+
+        position += movementOffset;
+
+        transform.position = controller.getFinalPosition(position);
+
+
+        if (connections.Count != controller.activePlayers.Count)
+        {
+
+            while (transform.childCount > 0)
+            {
+                DestroyImmediate(transform.GetChild(0).gameObject);
+            }
+
+            connections.Clear();
+            for (int i = 0; i < controller.activePlayers.Count; i++)
+            {
+                GameObject newConnection = Instantiate(connectionPrefab, transform.position, Quaternion.identity);
+                newConnection.transform.parent = transform;
+                connections.Add(newConnection.GetComponent<LineRenderer>());
+            }
+        }
+
+
+        totalInsideCircle = 0;
+        for (int i = 0; i < controller.activePlayers.Count; i++)
+        {
+            Vector3 dir = controller.activePlayers[i].transform.position - transform.position;
+            float dist = dir.magnitude;
+
+            if (dist < radiusForCollection)
+            {
+                totalInsideCircle++;
+                connections[i].positionCount = 2;
+                connections[i].SetPosition(0, transform.position);
+                connections[i].SetPosition(1, controller.activePlayers[i].transform.position);
+            }
+            else
+            {
+                connections[i].positionCount = 0;
+            }
+
+
+
+
+        }
+
 
     }
 
